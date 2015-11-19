d36115486a81667b9e92685e90c215ec2825a993
Felix Laura <felixgato8@gmail.com>
2015-11-18 18:37:51 -0800

    First commit.

---
+++ Spring4MVCHibernate4MysqlMavenIntegration/src/main/java/com/felixlaura/springmvc/dao/EmployeeDao.java
@@ -0,0 +1,14 @@
+package com.felixlaura.springmvc.dao;
+
+import java.util.List;
+
+import com.felixlaura.springmvc.model.Employee;
+
+public interface EmployeeDao {
+
+	Employee findById(int id);
+	void saveEmployee(Employee employee);
+	void deleteEmployeeBySsn(String ssn);
+	List<Employee> findAllEmployees();
+	Employee findEmployeeBySsn(String ssn);
+}
